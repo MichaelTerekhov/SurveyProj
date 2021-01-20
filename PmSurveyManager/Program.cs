db8281650e486d6713dfa01d7e20a99c2a186c0e
Michael Terekhov <mykhailo.terekhov@nure.ua>
2021-01-20 06:08:45 +0200

    Fully empty proj

---
+++ PmSurveyManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PmSurveyManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
